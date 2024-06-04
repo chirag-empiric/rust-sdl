b3f789aa500f4d1bdb2058293bdea80873faa0d7
chirag-empiric <chirag@empiricinfotech.com>
2024-06-04 19:10:09 +0530

    feat: user input using tree started

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::path::Path;
+mod input;
+use crate::input::Input;
 
 use sdl2::{
     event::Event,
@@ -6,10 +7,20 @@ use sdl2::{
     pixels::Color,
     rect::Rect,
     render::{TextureCreator, WindowCanvas},
+    sys::quad_t,
+    ttf::FontStyle,
     video::WindowContext,
 };
+use std::path::Path;
 
 fn main() -> Result<(), String> {
+    let mut userinput: Vec<char> = Vec::new();
+    let mut user_in = Input {
+        value: None,
+        next: Vec::new(),
+    };
+
+    // SDL Config - Move in another module
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
     let window = video_subsystem
@@ -23,11 +34,11 @@ fn main() -> Result<(), String> {
     let ttf_context = sdl2::ttf::init().map_err(|e| e.to_string())?;
     let font_path: &Path = Path::new(&"./src/montserrat/Montserrat-VariableFont_wght.ttf");
 
-    let font = ttf_context.load_font(font_path, 128).unwrap();
+    let mut font = ttf_context.load_font(font_path, 128).unwrap();
+    font.set_style(FontStyle::BOLD);
     let mut event_pump = sdl.event_pump().unwrap();
 
-    let mut userinput: Vec<char> = Vec::new();
-
+    // Infinite loop to keep display open
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
@@ -46,6 +57,16 @@ fn main() -> Result<(), String> {
                     userinput.push(ch[0]);
                     let str = String::from_iter(userinput.iter());
                     let _ = render(&mut canvas, &texture_creator, &font, &str);
+
+                    // regarding tree
+                    if user_in.value == None {
+                        user_in.value = Some(ch[0]);
+                    } else {
+                        let index_to_push = user_in.next.len();
+                        let _ = Input::push_trailing(&mut user_in, ch[0], index_to_push);
+                    }
+
+                    println!("User input is: {:?}", user_in);
                 }
                 Event::KeyDown {
                     keycode: Some(Keycode::Backspace),
@@ -55,7 +76,23 @@ fn main() -> Result<(), String> {
                     userinput.pop();
                     let str = String::from_iter(userinput.iter());
                     let _ = render(&mut canvas, &texture_creator, &font, &str);
+                    // regarding tree
                 }
+                Event::KeyUp {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => {
+                    //
+                    println!("Back arrow key")
+                }
+                Event::KeyUp {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => {
+                    //
+                    println!("Forwared arrow key")
+                }
+
                 _ => {}
             }
         }
@@ -75,14 +112,14 @@ fn render(
 
     let surface = font
         .render(input)
-        .blended(Color::RGB(255, 0, 0))
+        .blended(Color::RGB(0, 0, 0))
         .map_err(|e| e.to_string())?;
 
     let texture = texture_creator
         .create_texture_from_surface(&surface)
         .map_err(|e| e.to_string())?;
 
-    let target = Rect::new(100 as i32, 0 as i32, 200 as u32, 100 as u32);
+    let target = Rect::new(100 as i32, 0 as i32, 60 as u32, 60 as u32);
     canvas.copy(&texture, None, Some(target))?;
     canvas.present();
     Ok(())
