91c4241085c6fddfbab90131284a5691400d67c5
chirag-empiric <chirag@empiricinfotech.com>
2024-06-06 14:47:37 +0530

    refactor: rect width dynamic

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,6 @@ use sdl2::{
     pixels::Color,
     rect::Rect,
     render::{TextureCreator, WindowCanvas},
-    sys::quad_t,
     ttf::FontStyle,
     video::WindowContext,
 };
@@ -15,6 +14,7 @@ use std::path::Path;
 
 fn main() -> Result<(), String> {
     let mut userinput: Vec<char> = Vec::new();
+    let mut curent_cursor_index = 0;
     let mut user_in = Input {
         value: None,
         next: Vec::new(),
@@ -24,7 +24,7 @@ fn main() -> Result<(), String> {
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
     let window = video_subsystem
-        .window("Learninggggggg", 600, 600)
+        .window("Learninggggggg", 1000, 800)
         .position_centered()
         .build()
         .unwrap();
@@ -37,6 +37,7 @@ fn main() -> Result<(), String> {
     let mut font = ttf_context.load_font(font_path, 128).unwrap();
     font.set_style(FontStyle::BOLD);
     let mut event_pump = sdl.event_pump().unwrap();
+    let mut old = 35;
 
     // Infinite loop to keep display open
     'running: loop {
@@ -56,14 +57,17 @@ fn main() -> Result<(), String> {
                     let ch: Vec<char> = text.chars().collect();
                     userinput.push(ch[0]);
                     let str = String::from_iter(userinput.iter());
-                    let _ = render(&mut canvas, &texture_creator, &font, &str);
+                    let old_val =
+                        render(&mut canvas, &texture_creator, &font, &str, old, Keycode::A);
 
+                    old = old_val.unwrap();
+                    println!("old value: {}", old);
                     // regarding tree
                     if user_in.value == None {
                         user_in.value = Some(ch[0]);
                     } else {
-                        let index_to_push = user_in.next.len();
-                        let _ = Input::push_trailing(&mut user_in, ch[0], index_to_push);
+                        let _ = Input::push_trailing(&mut user_in, ch[0], curent_cursor_index);
+                        curent_cursor_index += 1;
                     }
 
                     println!("User input is: {:?}", user_in);
@@ -75,7 +79,15 @@ fn main() -> Result<(), String> {
                     //
                     userinput.pop();
                     let str = String::from_iter(userinput.iter());
-                    let _ = render(&mut canvas, &texture_creator, &font, &str);
+                    let old_val = render(
+                        &mut canvas,
+                        &texture_creator,
+                        &font,
+                        &str,
+                        old,
+                        Keycode::Backspace,
+                    );
+                    old = old_val.unwrap_or_else(|_| 20);
                     // regarding tree
                 }
                 Event::KeyUp {
@@ -83,7 +95,7 @@ fn main() -> Result<(), String> {
                     ..
                 } => {
                     //
-                    println!("Back arrow key")
+                    println!("Back arrow");
                 }
                 Event::KeyUp {
                     keycode: Some(Keycode::Right),
@@ -105,22 +117,32 @@ fn render(
     texture_creator: &TextureCreator<WindowContext>,
     font: &sdl2::ttf::Font,
     input: &str,
-) -> Result<(), String> {
-    let color = Color::RGB(255, 255, 255);
+    old: u32,
+    operation: Keycode,
+) -> Result<u32, String> {
+    let color = Color::WHITE;
     canvas.set_draw_color(color);
     canvas.clear();
 
     let surface = font
         .render(input)
-        .blended(Color::RGB(0, 0, 0))
+        .blended(Color::BLACK)
         .map_err(|e| e.to_string())?;
 
     let texture = texture_creator
         .create_texture_from_surface(&surface)
         .map_err(|e| e.to_string())?;
 
-    let target = Rect::new(100 as i32, 0 as i32, 60 as u32, 60 as u32);
+    let target;
+    if operation == Keycode::Backspace {
+        target = Rect::new(100 as i32, 0 as i32, old - 20 as u32, 40 as u32);
+        canvas.copy(&texture, None, Some(target))?;
+        canvas.present();
+        return Ok(old - 20);
+    } else {
+        target = Rect::new(100 as i32, 0 as i32, old + 20 as u32, 40 as u32);
+    }
     canvas.copy(&texture, None, Some(target))?;
     canvas.present();
-    Ok(())
+    Ok(old + 20)
 }
