d82c4d3201e5dd42ec021d978e06ac046bdb26c4
chirag-empiric <chirag@empiricinfotech.com>
2024-06-18 14:45:27 +0530

    feat: split recursive

--- cli-editor/src/ds/mod.rs
+++ cli-editor/src/ds/mod.rs
@@ -31,9 +31,6 @@ impl Rope {
 
     pub fn concate(left: Rope, right: Rope) -> Result<Self, Error> {
         let left_wt = left.weight();
-        let right_wt = right.weight();
-
-        println!("weight: {:?}", left_wt + right_wt);
 
         let new_root = Self {
             root: RopeNode::Internal {
@@ -77,6 +74,84 @@ impl Rope {
         match &self.root {
             RopeNode::Leaf { val, .. } => val.len(),
             RopeNode::Internal { weight, .. } => *weight,
+        }
+    }
+
+    pub fn traverse(&self) -> Result<(), Error> {
+        self.read_recursive(&self.root);
+        Ok(())
+    }
+
+    pub fn read_recursive(&self, node: &RopeNode) {
+        match node {
+            RopeNode::Leaf { val, .. } => {
+                print!("{}", val);
+            }
+
+            RopeNode::Internal { left, right, .. } => {
+                self.read_recursive(&left);
+                self.read_recursive(&right);
+            }
+        }
+    }
+
+    pub fn split_from_idx(&self, index: usize) -> (Option<RopeNode>, Option<RopeNode>) {
+        println!("index: {}", index);
+        let (left, right) = self.split_recursive(&self.root, index);
+
+        (Some(left.unwrap()), Some(right.unwrap()))
+    }
+
+    fn split_recursive(
+        &self,
+        node: &RopeNode,
+        index: usize,
+    ) -> (Option<RopeNode>, Option<RopeNode>) {
+        match node {
+            RopeNode::Leaf { val, .. } => {
+                let left = RopeNode::Leaf {
+                    val: val[..index].to_string(),
+                    weight: val[..index].to_string().len(),
+                };
+                let right = RopeNode::Leaf {
+                    val: val[index..].to_string(),
+                    weight: val[index..].to_string().len(),
+                };
+
+                println!(
+                    "value in left: {:?} \n in right: {:?}",
+                    val[..index].to_string(),
+                    val[index..].to_string()
+                );
+                (Some(left), Some(right))
+            }
+            RopeNode::Internal {
+                left,
+                right,
+                weight,
+            } => {
+                //
+                if index < *weight {
+                    let (l, r) = self.split_recursive(&left, index);
+                    let new_right = RopeNode::Internal {
+                        left: Box::new(r.unwrap()),
+                        right: right.clone(),
+                        weight: weight - index,
+                    };
+
+                    (l, Some(new_right))
+                } else {
+                    //
+                    let (l, r) = self.split_recursive(&right, index - weight);
+                    let new_left = RopeNode::Internal {
+                        left: left.clone(),
+                        right: Box::new(l.unwrap()),
+                        weight: index - weight,
+                    };
+
+                    (Some(new_left), r)
+                }
+            }
         }
     }
 }
--- cli-editor/src/main.rs
+++ cli-editor/src/main.rs
@@ -3,20 +3,25 @@ mod ds;
 // use editor::Editor;
 
 fn main() {
-    let node1 = ds::Rope::new("Hello").unwrap();
-    let node2 = ds::Rope::new("World").unwrap();
-    let node3 = ds::Rope::new("World").unwrap();
+    let node1 = ds::Rope::new("Hello ").unwrap();
+    let node2 = ds::Rope::new("World ").unwrap();
+    let node3 = ds::Rope::new("Chirag ").unwrap();
+    let node4 = ds::Rope::new("Jani").unwrap();
 
-    println!("{node1:?}");
-    println!("{node2:?}");
+    let concat1 = ds::Rope::concate(node1, node2).unwrap();
 
-    let old = ds::Rope::concate(node1, node2).unwrap();
+    let concat2 = ds::Rope::concate(node3, node4).unwrap();
 
-    let ans = ds::Rope::concate(old, node3).unwrap();
-    println!("{ans:?}");
+    let concat3 = ds::Rope::concate(concat1, concat2).unwrap();
 
-    let traverse = ans.at_index(3).unwrap();
-    println!("Found char: {traverse:?}");
+    let _find_at_index = concat3.at_index(3).unwrap();
+
+    let _ = concat3.traverse();
+    println!(); // for line break
+
+    let _split1 = concat3.split_from_idx(3);
+    println!("Left {:?}", _split1.0);
+    println!("Right {:?}", _split1.1);
 
     /* let mut editor = Editor::default();
     editor.run(); */
